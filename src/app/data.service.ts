f317ce527eed1458cc8c3a21c1f4b335d4d76b33
akrawczyk <agammkrawczyk@gmail.com>
2020-04-05 18:46:49 +0200

    #symulating loading deata with spiner

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
 
   getPlanets(){
-    return this.httpClient.get('assets/planets.json');
+    return this.httpClient.get('assets/planets.json')
+      .pipe(
+        delay(2000)
+      );
   }
 }
